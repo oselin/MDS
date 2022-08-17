2e971c418744b53483617852471a7e5df6a3410b
oselin <pierfrancesco.oselin@gmail.com>
2022-08-17 09:41:45 +0200

    Working on the library in a separated repo

---
+++ UAV/__init__.py
@@ -0,0 +1,16 @@
+from UAV.Robot   import Robot
+from UAV.Algebra import remove_offset
+from UAV.Algebra import move
+from UAV.Algebra import DM_from_platoon
+from UAV.Algebra import DM_from_platoon2
+from UAV.Algebra import DM_from_S
+from UAV.Algebra import DM_from_S2
+from UAV.Algebra import rotateMatrix
+from UAV.Algebra import get_theta
+from UAV.Algebra import EVD
+from UAV.Algebra import MDS
+from UAV.Algebra import noise_matrix
+from UAV.Algebra import square
+from UAV.Algebra import expected_value
+
+from UAV.Plots   import plot_points
